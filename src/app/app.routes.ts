6bac3552b393da86cf4630ebc29b2ccc28640496
17abiba <17abiba@gmail.com>
2024-12-15 14:09:28 +0200

    push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './screenOne/about/about.component';
+import { NotfoundComponent } from './screenOne/notfound/notfound.component';
+import { ContactusComponent } from './screenOne/contactus/contactus.component';
+import { LoginComponent } from './screenOne/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:"about",pathMatch:"full"},
+    {path:"about",component:AboutComponent},
+    {path:"contactus",component:ContactusComponent},
+    {path:"login",component:LoginComponent},
+    {path:'**',component:NotfoundComponent}
+];
