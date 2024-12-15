89f9f521465b59ab977e9a4e0cbc5e90001ab551
MostafaM117 <mostafa.hamed1173@gmail.com>
2024-12-15 14:59:26 +0200

    connect firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { initializeApp } from 'firebase/app';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Iaems';
+  firebaseConfig = {
+    apiKey: "AIzaSyD_3e0jaoYADzbFLTl0Qy8_mI9FkHfpH1A",
+    authDomain: "iaems-angular-4b1fd.firebaseapp.com",
+    projectId: "iaems-angular-4b1fd",
+    storageBucket: "iaems-angular-4b1fd.firebasestorage.app",
+    messagingSenderId: "678265456391",
+    appId: "1:678265456391:web:6749e592ed74b2d69227a2"
+  };
+  app = initializeApp(this.firebaseConfig);
 }
