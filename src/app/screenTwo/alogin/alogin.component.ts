6fcdaa5aa45a8a0b651d8b1c5ddc2cb29008daf0
MostafaM117 <mostafa.hamed1173@gmail.com>
2024-12-16 06:26:18 +0200

    my upload

--- src/app/screenTwo/alogin/alogin.component.ts
+++ src/app/screenTwo/alogin/alogin.component.ts
@@ -3,11 +3,12 @@ import { FooterComponent } from "../../screenOne/shared/footer/footer.component"
 import { AnavComponent } from "../anav/anav.component";
 import { AccountComponent } from "../section_4/account/account.component";
 import { DashboardComponent } from "../section_1/dashboard/dashboard.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-alogin',
   standalone: true,
-  imports: [FooterComponent, AnavComponent, AccountComponent, DashboardComponent],
+  imports: [FooterComponent, AnavComponent, RouterModule, AccountComponent, DashboardComponent],
   templateUrl: './alogin.component.html',
   styleUrl: './alogin.component.css'
 })
